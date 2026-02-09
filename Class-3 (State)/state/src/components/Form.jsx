b7acf0325d78d55d36a1a6792185ae344ae94e0d
mrinal1224 <iammrinalkc@gmail.com>
2026-02-09 12:17:00 +0530

    form handling

--- Class-3 (State)/state/src/components/Form.jsx
+++ Class-3 (State)/state/src/components/Form.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  function handleSignup() {
+    let userObj = {
+      name: name,
+      email: email,
+      pass: password,
+    };
+
+    console.log(userObj);
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div>
       <h1>Sign Up Form</h1>
 
       <label>Name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
       <label>Email</label>
-      <input type="text" />
+      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
 
       <label>Password</label>
-      <input type="text" />
+      <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-      <button>Submit</button>
+      <button onClick={handleSignup}>Submit</button>
     </div>
   );
 }
