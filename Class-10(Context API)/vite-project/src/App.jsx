d22152489a0d06396f8075092e20033c855a05ad
mrinal1224 <iammrinalkc@gmail.com>
2026-03-16 11:36:12 +0530

    using context at main

--- Class-10(Context API)/vite-project/src/App.jsx
+++ Class-10(Context API)/vite-project/src/App.jsx
@@ -4,31 +4,10 @@ import "./App.css";
 import { ParkContext } from "./components/ParkContext";
 
 function App() {
-  const parkInfo = {
-    parkName: "Imagica Amusement park",
-
-    rollerCoaster: "Must be taller than 48 inches",
-    waterSlide: "You must know how to Swim",
-    merryGoRound: "Children must have age less than 10",
-
-    ticketForRollerCoaster: () => {
-      return "RollerCoaster Started";
-    },
-
-    ticketforWaterSlide: () => {
-      return "WaterSlide Started";
-    },
-
-    ticketForMerryGoRound: () => {
-      return "Merry go Round Started";
-    },
-  };
   return (
     <>
-      <ParkContext.Provider value={parkInfo}>
-        <Parent1 />
-        <Parent2 />
-      </ParkContext.Provider>
+      <Parent1 />
+      <Parent2 />
     </>
   );
 }
--- Class-10(Context API)/vite-project/src/main.jsx
+++ Class-10(Context API)/vite-project/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ParkContext } from "./components/ParkContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const parkInfo = {
+  parkName: "Imagica Amusement park",
+
+  rollerCoaster: "Must be taller than 48 inches",
+  waterSlide: "You must know how to Swim",
+  merryGoRound: "Children must have age less than 10",
+
+  ticketForRollerCoaster: () => {
+    return "RollerCoaster Started";
+  },
+
+  ticketforWaterSlide: () => {
+    return "WaterSlide Started";
+  },
+
+  ticketForMerryGoRound: () => {
+    return "Merry go Round Started";
+  },
+};
+
+createRoot(document.getElementById("root")).render(
+  <ParkContext.Provider value={parkInfo}>
     <App />
-  </StrictMode>,
-)
+  </ParkContext.Provider>
+);
