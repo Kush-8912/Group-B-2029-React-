32a330578f2d8aeaed46ab0be4669d4ece95d1fe
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-04-06 11:14:19 +0530

    Create Memo.jsx

---
+++ Memo.jsx
@@ -0,0 +1,27 @@
+import React, { useMemo, useState } from "react";
+
+function Memo() {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(1);
+
+  function doubleValue(num) {
+    for (let i = 0; i < 1000000000; i++) {
+      /// some processing
+    }
+    return num * 2;
+  }
+
+  const doubledValue = useMemo(() => doubleValue(number), [number]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>{count}</h1>
+
+      <input value={number} onChange={(e) => setNumber(e.target.value)} />
+      <h3>Doubled Value :{doubledValue} </h3>
+    </div>
+  );
+}
+
+export default Memo;
